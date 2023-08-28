2fb6da2ecd4c962b40b40bed8d8576e39a5c8a1b
Saikumar Nalla <ksai92839@gmail.com>
2023-08-28 19:17:08 -0400

    add resume button and styling

--- src/Components/Homepage/Aboutme.js
+++ src/Components/Homepage/Aboutme.js
@@ -7,6 +7,7 @@ return (
     <div className="aboutme"> 
         <h1 className="h1">Hello!</h1>
         <h2 className="h2"> Here's who I am & what I do</h2>
+        <a href="https://www.google.com" className="resume">Resume</a>
         <p className="p">Virat Kohli is an Indian international cricketer and the former captain of the Indian national cricket team.</p>
 
     </div>
