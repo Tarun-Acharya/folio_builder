9e3cb7d8a5c0395c4b86a469016f8216bfcadf68
Tarun Acharya <tarunkodakandla@gmail.com>
2023-09-02 15:18:14 -0400

    ta0902 Remove position absolute for navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import NavBar from "./Components/NavBar/NavBar";
+import Work from "./Components/Work/Work";
 function App() {
   //const details=[{pic: picture, name: "Virat", role: "Cricketer"}];
   return (
@@ -9,7 +10,9 @@ function App() {
       <NavBar />
       <div className="AppElements">
         <Routes>
+          <Route path="" Component={Homepage}></Route>
           <Route path="/Home" Component={Homepage}></Route>
+          <Route path="/Work" Component={Work}></Route>
         </Routes>
       </div>
     </div>
