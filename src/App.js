1f43b67771d8ec6f7062d5066309b97055999740
Saikumar Nalla <ksai92839@gmail.com>
2023-09-02 15:21:33 -0400

    add ProjectsMainPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Homepage from "./Components/Homepage/Homepage";
 import NavBar from "./Components/NavBar/NavBar";
+import MainProjectPage from "./Components/Projects/MainProjectPage";
 function App() {
   //const details=[{pic: picture, name: "Virat", role: "Cricketer"}];
   return (
@@ -10,6 +11,7 @@ function App() {
       <div className="AppElements">
         <Routes>
           <Route path="/Home" Component={Homepage}></Route>
+          <Route path="/Projects" element={<MainProjectPage/>}></Route>
         </Routes>
       </div>
     </div>
